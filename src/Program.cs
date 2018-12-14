926a30bcbc1f318a1b91e7fe3e172bc92a1c2d5d
menalb <menalb84@gmail.com>
2018-12-14 09:44:45 +0000

    initial project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScheduledMessageConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
